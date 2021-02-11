f49d747c05aed5b4fe3843b094b71b598a15cd6e
Joe Daywalt <epiblitikos@users.noreply.github.com>
2021-02-10 21:49:16 -0500

    Refactored cutter.gd
    renamed stuff and broke down the big nested set of loops

--- addons/BitmapFontCutterPlus/cutter.gd
+++ addons/BitmapFontCutterPlus/cutter.gd
@@ -1,89 +1,110 @@
 tool
 extends BitmapFont
 
+
 var index = 0
-export(Vector2) var GlyphSize = Vector2(8,8) setget changeGlyphSize
-export(Texture) var TextureToCut = null setget changeTexture
-export(int,255) var StartChar = 32 setget changeStartChar
-export(float) var Spacing = 1 setget changeSpacing
-export(bool) var Monospaced = true setget changeMonospaced
+export(Vector2) var glyph_size = Vector2(8,8) setget change_glyph_size
+export(Texture) var texture_to_cut = null setget change_texture
+export(int, 255) var start_char = 32 setget change_start_char
+export(float) var spacing = 1 setget change_spacing
+export(bool) var monospaced = true setget change_monospaced
 
-func changeStartChar(value):
-	StartChar = value
-	update()
 
-func changeGlyphSize(value):
-	GlyphSize = value
-	height = GlyphSize.y
-	update()
-	
-func changeTexture(value):
-	TextureToCut = value
-	index = 0
-	if TextureToCut:
-		update()
-	
-func changeSpacing(value):
-	Spacing = value
-	update()
+func change_start_char(value):
+    start_char = value
+    update()
 
-func changeMonospaced(value):
-	Monospaced = value
-	update()
 
+func change_glyph_size(value):
+    glyph_size = value
+    height = glyph_size.y
+    update()
+    
+
+func change_texture(value):
+    texture_to_cut = value
+    index = 0
+    if texture_to_cut:
+        update()
+    
+
+func change_spacing(value):
+    spacing = value
+    update()
+
+
+func change_monospaced(value):
+    monospaced = value
+    update()
+
+
+func _get_char_region(col, row):
+    var left_margin = 0
+    var character_width = glyph_size.x
+    
+    if not monospaced:
+        var data = texture_to_cut.get_data()
+        data.lock()
+        
+        var found = false
+        for px in range(glyph_size.x):
+            if found:
+                break
+            for py in range(glyph_size.y):
+                var pixel = data.get_pixel(
+                    col * glyph_size.x + px,
+                    row * glyph_size.y + py
+                )
+                if pixel.a != 0:
+                    left_margin = px
+                    character_width -= px
+                    found = true
+                    break
+        
+        found = false
+        for px in range(glyph_size.x):
+            if found:
+                break
+            for py in range(glyph_size.y):
+                var pixel = data.get_pixel(
+                    col * glyph_size.x + glyph_size.x - px - 1,
+                    row * glyph_size.y + py
+                )
+                if pixel.a != 0:
+                    character_width -= px
+                    found = true
+                    break
+                    
+        data.unlock()
+    
+    return Rect2(
+        col * glyph_size.x + left_margin,
+        row * glyph_size.y,
+        character_width,
+        glyph_size.y
+    )
+    
+    
 func update():
-	print("Cut texture to font")
-	if TextureToCut != null:
-		if GlyphSize.x > 0 and GlyphSize.y > 0:
-			
-			var w  = TextureToCut.get_width()
-			var h  = TextureToCut.get_height()
-			var tx = w / GlyphSize.x
-			var ty = h / GlyphSize.y
+    print("CutBitmapFont: Cut texture to font")
+    if texture_to_cut != null and glyph_size.x > 0 and glyph_size.y > 0:
+        var chars_wide = texture_to_cut.get_width() / glyph_size.x
+        var chars_high = texture_to_cut.get_height() / glyph_size.y
+        var char_offset = 0
 
-			var font = self
-			var i = 0  #Iterator for char index
-
-			clear()
-
-			#Begin cutting..... so edgy
-			font.add_texture(TextureToCut)
-			font.height = GlyphSize.y
-			for y in range(ty):
-				for x in range(tx):
-					var l = 0
-					var character_width = GlyphSize.x
-					
-					if !Monospaced:
-						var data = TextureToCut.get_data()
-						data.lock()
-						
-						var found = false
-						for xx in range(0,GlyphSize.x):
-							if found: break
-							for yy in range(0,GlyphSize.y):
-								var pixel = data.get_pixel(x*GlyphSize.x + xx, y*GlyphSize.y + yy)
-								if pixel.a != 0:
-									l = xx
-									character_width -= xx
-									found = true
-									break
-						
-						found = false
-						for xx in range(0,GlyphSize.x):
-							if found: break
-							for yy in range(0,GlyphSize.y):
-								var pixel = data.get_pixel(x*GlyphSize.x + GlyphSize.x - xx -1, y*GlyphSize.y + yy)
-								if pixel.a != 0:
-									character_width -= xx
-									found = true
-									break
-									
-						data.unlock()
-					
-					var region = Rect2(x*GlyphSize.x + l,y*GlyphSize.y,character_width, GlyphSize.y)
-					font.add_char(StartChar + i, 0, region, Vector2.ZERO, character_width + Spacing)
-						
-					i+=1
-			update_changes()
-	pass #if texture is null
+        clear()
+        add_texture(texture_to_cut)
+        height = glyph_size.y
+        
+        for row in range(chars_high):
+            for col in range(chars_wide):
+                var region = _get_char_region(col, row)
+                add_char(
+                    start_char + char_offset,
+                    0,
+                    region,
+                    Vector2.ZERO,
+                    region.size[0] + spacing
+                )
+                char_offset += 1
+        update_changes()
