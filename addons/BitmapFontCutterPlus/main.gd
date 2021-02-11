cd654c22eecf081a13c1b43a332d5c05beb0adaa
Joe Daywalt <epiblitikos@users.noreply.github.com>
2021-02-10 21:51:16 -0500

    Refactored main.gd
    Mostly just line length. Names were decent in this one.

--- addons/BitmapFontCutterPlus/main.gd
+++ addons/BitmapFontCutterPlus/main.gd
@@ -3,7 +3,13 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("CutBitmapFont","BitmapFont",preload("cutter.gd"),preload("icon.png"))
-	
+    add_custom_type(
+        "CutBitmapFont",
+        "BitmapFont",
+        preload("cutter.gd"),
+        preload("icon.png")
+    )
+    
+    
 func _exit_tree():
-	remove_custom_type("CutBitmapFont")
+    remove_custom_type("CutBitmapFont")
